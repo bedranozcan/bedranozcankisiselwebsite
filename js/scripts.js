4e20db03485c21fd6723af53ca13b911e6a16f16
bedranozcan <bedra@IDVLABS>
2023-09-12 19:15:22 +0300

    İletisim sayfası düzenlendi.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,40 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contactForm");
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault(); // Formun otomatik olarak gönderilmesini engelle
+
+        // Formdan değerleri al
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const phone = document.getElementById("phone").value;
+        const message = document.getElementById("message").value;
+
+        // Formu oluştur
+        const formData = new FormData();
+        formData.append("name", name);
+        formData.append("email", email);
+        formData.append("phone", phone);
+        formData.append("message", message);
+
+        // XMLHttpRequest ile veriyi gönder
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "sendmail.php");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    // Başarıyla gönderildiğinde burası çalışır
+                    document.getElementById("submitSuccessMessage").classList.remove("d-none");
+                    contactForm.reset(); // Formu sıfırla
+                } else {
+                    // Hata oluştuğunda burası çalışır
+                    document.getElementById("submitErrorMessage").classList.remove("d-none");
+                }
+                document.getElementById("submitButton").classList.remove("disabled");
+            }
+        };
+        xhr.send(formData);
+
+        document.getElementById("submitButton").classList.add("disabled");
+    });
+});
